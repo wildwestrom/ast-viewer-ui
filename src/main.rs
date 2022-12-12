06e5f3e5328ae3836bbc22a572d66d172a2df2b3
Christian Westrom <c.westrom@westrom.xyz>
2022-12-12 11:17:30 -0500

    successfully round trip a simple file

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 use std::{ffi::OsString, error::Error};
-
 use clap::Parser;
-
-// proc-macro-2 crate might come in handy
+use quote::ToTokens;
+// proc-macro2 crate might come in handy
 
 fn main() -> Result<(), Box<dyn Error>> {
 	let args = Cli::parse();
-	let filename = args.file;
-	let contents = std::fs::read_to_string(filename)?;
+	let contents = std::fs::read_to_string(args.input)?;
+    // turn file into ast
     let ast = syn::parse_file(&contents)?;
-    println!("{:#?}", ast);
+    // turn ast back into a file
+    let str_from_ast: String = ast.items.iter().map(|item| {
+        let token_stream = item.to_token_stream().to_string();
+        token_stream
+    }).collect();
+    std::fs::write(args.output, str_from_ast)?;
     Ok(())
 }
 
 #[derive(Parser)]
 struct Cli {
 	/// File to browse ast of.
-	file: OsString,
+	input: OsString,
+    /// File to output to.
+    #[clap(short, long, default_value = "out.rs")]
+    output: OsString,
 }
 
 #[cfg(test)]
