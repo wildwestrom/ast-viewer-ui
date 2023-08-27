ddb2b647f5aaf809444ddf87c4eb37ae8bf96d40
Christian Westrom <c.westrom@westrom.xyz>
2023-08-27 14:31:07 +0900

    test that the output ast will generate the same input code
    It currently doesn't work because there is no way in `syn` to parse a comment other than a doc comment.

--- parse_rust/src/lib.rs
+++ parse_rust/src/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::{anyhow, Result};
 use quote::ToTokens;
 pub use syn;
 
@@ -28,6 +29,31 @@ pub fn round_trip(input_code: &str) -> syn::Result<String> {
 	Ok(ast_to_string(ast))
 }
 
+/// This was partly copy pasted from rust-analyzer.
+///
+/// # Warning
+///
+/// This function relies on a shell command and thus any environment in which the program executes.
+pub fn format_rust_code(input_code: &str) -> Result<String> {
+	let mut cmd = std::process::Command::new("rustfmt");
+
+	let mut fmt = cmd
+		.stdin(std::process::Stdio::piped())
+		.stdout(std::process::Stdio::piped())
+		.stderr(std::process::Stdio::piped())
+		.spawn()?;
+
+	std::io::Write::write_all(
+		&mut fmt.stdin.as_mut().ok_or(anyhow!("No Stdin"))?,
+		input_code.as_bytes(),
+	)?;
+
+	let output = fmt.wait_with_output()?;
+	let captured_stdout = String::from_utf8(output.stdout)?;
+
+	Ok(captured_stdout)
+}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
@@ -41,14 +67,23 @@ mod tests {
 	];
 
 	#[test]
-	fn test_round_trip() {
+	fn round_trip_generates_equivalent_ast() {
 		for case in TEST_CASES {
 			let filepath = format!("../test-inputs/{}", case);
-			let contents = std::fs::read_to_string(filepath.clone())
+			let input = std::fs::read_to_string(filepath.clone())
 				.unwrap_or_else(|_| panic!("failed to read file from {}", filepath));
-			let roundtripped_code =
-				round_trip(&contents).expect("failed to read rust source string");
+			let roundtripped_code = round_trip(&input).expect("failed to read rust source string");
 			assert_eq!(round_trip(&roundtripped_code).unwrap(), roundtripped_code);
+		}
+	}
+	#[test]
+	fn output_from_ast_is_equal_to_input() {
+		for case in TEST_CASES {
+			let filepath = format!("../test-inputs/{}", case);
+			let input = std::fs::read_to_string(filepath.clone())
+				.unwrap_or_else(|_| panic!("failed to read file from {}", filepath));
+			let output = format_rust_code(&round_trip(&input).unwrap()).unwrap();
+			assert_eq!(input, output);
 		}
 	}
 }
